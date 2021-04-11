5bb3c5945acaa94733099b054159bb5a7a8e66e7
Sergey Petrenko <gray@searchengines.ru>
2021-04-11 17:24:39 +0300

    changes

--- entrypoint.sh
+++ entrypoint.sh
@@ -21,7 +21,7 @@ chmod 600 "${HOME}/.ssh/id_rsa_deploy.pub"
 rsync --version
 sh -c "
 rsync $2 \
-  -e 'ssh -o ${HOME}/.ssh/id_rsa_deploy -o StrictHostKeyChecking=no' \
+  -e 'ssh -i ${HOME}/.ssh/id_rsa_deploy -o StrictHostKeyChecking=no' \
   ${GITHUB_WORKSPACE}/public \
   ${VPS_DEPLOY_USER}@${VPS_DEPLOY_HOST}:${VPS_DEPLOY_DEST}
 "
